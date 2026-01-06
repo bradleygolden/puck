ef434ef9ac7a08878126d9033b16e7058d18b45d
Bradley Golden <golden.bradley@gmail.com>
2026-01-06 10:01:19 -0600

    Add deps.unlock, deps.audit, and sobelow to precommit checks

--- mix.exs
+++ mix.exs
@@ -56,7 +56,8 @@ defmodule Puck.MixProject do
       {:ex_doc, "~> 0.34", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false}
+      {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false}
     ]
   end
 
@@ -67,7 +68,10 @@ defmodule Puck.MixProject do
         "test --warnings-as-errors",
         "format --check-formatted",
         "credo --strict",
-        "dialyzer"
+        "dialyzer",
+        "deps.unlock --check-unused",
+        "deps.audit",
+        "sobelow --config"
       ]
     ]
   end
